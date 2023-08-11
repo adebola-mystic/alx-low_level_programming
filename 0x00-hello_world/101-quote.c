76accdb033e4ca1514a3b06cf125cc445dcf2d4d
adebola-mystic <adebolasonofadam@gmail.com>
2023-08-10 19:29:24 -0700

    UNIX is a simple OS

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,11 @@
+#include <stdio.h>
 #include <unistd.h>
-
-int main(void) {
-    // Write the desired string to the standard error
-    char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    write(2, msg, sizeof(msg) - 1);
-
-    // Return 1 as required
-    return 1;
+/**
+ * main - A program that prints standard errror
+ * Return 1(Success)
+ */
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout)'
+	return (1);
 }
