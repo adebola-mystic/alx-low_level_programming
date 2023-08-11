725a95acbe821690313c1cd43589dc45a86eb56c
adebola-mystic <adebolasonofadam@gmail.com>
2023-08-10 18:11:34 -0700

    Hello print f

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio>
+/**
+ * main - A program that print using printf f instead of put
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art, \n");
+		return (0);
+}
