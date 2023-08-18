da77adf247805be3230a781a3973556043626093
adebola-mystic <adebolasonofadam@gmail.com>
2023-08-18 15:38:50 -0700

    Multiply two integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - multiplication of two integers
+ * @a: The first integer
+ * @b: The second integer
+ * Return: 0 Success
+ */
+
+int mul(int a, int b)
+{
+int ans = a * b;
+return (ans);
+}
